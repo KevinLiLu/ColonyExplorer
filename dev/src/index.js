34cee66469c20b646bbda2527bedd39a84b1749c
lu.kevin@berkeley.edu <kelu@paypal.com>
2018-06-17 15:16:38 -0700

    delete unneeded dev directory

--- dev/src/index.js
+++
@@ -1,8 +0,0 @@
-const createColony = require('./create_colony');
-const createTask = require('./create_task');
-
-createColony()
-  .then(createTask)
-  // We're exiting hard here as the providers keep polling otherwise
-  .then(() => process.exit())
-  .catch(err => console.error(err));
